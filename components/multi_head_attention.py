fix: fix attention calculation

components/multi_head_attention.py
@@ -73,4 +73,5 @@ class MultiHeadAttention(tf.keras.Model):
 
     def _calc_attention(self, query, value, key):
         scores = tf.matmul(query, key, transpose_b=True)
-        return tf.linalg.matmul(tf.nn.softmax(scores / key.shape[-1], axis=1), value)
+        return tf.linalg.matmul(tf.nn.softmax(scores / tf.math.sqrt(key.shape[-1]),
+                                              axis=1), value)